a77b6b2b6b59a9faacb3c077e35f939440276c97
Laurence Green <vesper.porta@gmail.com>
2018-07-05 17:44:21 +0100

    feature(migrations): Update migrations for published object changes

--- books/serializers_publish.py
+++ books/serializers_publish.py
@@ -2,8 +2,6 @@
 
 from rest_framework import serializers
 
-from books.models import BookReview
-
 
 class BookReviewPublishSerializer(serializers.ModelSerializer):
     """BookReview publishable serializer."""
@@ -13,21 +11,9 @@ class BookReviewPublishSerializer(serializers.ModelSerializer):
         read_only=True,
         view_name='bookreview-detail',
     )
-    book = serializers.HyperlinkedRelatedField(
-        many=False,
-        view_name='book-detail',
-        queryset=Book.objects.all(),
-    )
-    progress = serializers.HyperlinkedRelatedField(
-        many=False,
-        view_name='bookprogress-detail',
-        queryset=BookProgress.objects.all(),
-        required=False,
-        allow_null=True,
-    )
 
     class Meta:
-        model = BookReview
+        model = 'books.models.BookReview'
         read_only_fields = (
             'id',
             'created_at',
@@ -35,8 +21,6 @@ class BookReviewPublishSerializer(serializers.ModelSerializer):
             'type',
             'copy',
             'rating',
-            'book',
-            'progress',
         )
         fields = read_only_fields
         exclude = []
