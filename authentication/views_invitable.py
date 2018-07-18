085acb44c0144d07f16f8525e66c486ca5d624d5
Laurence Green <vesper.porta@gmail.com>
2018-07-18 13:53:48 +0100

    feature(permissions): Added permissions specific for Invitations and their custom viewset mixin.

--- authentication/views_invitable.py
+++ authentication/views_invitable.py
@@ -1,7 +1,7 @@
 """Invitation mixin views."""
 
-from rest_framework import status
-from rest_framework.decorators import detail_route
+from rest_framework import (status, permissions)
+from rest_framework.decorators import (detail_route, permission_classes)
 from rest_framework.response import Response
 
 from authentication.models import Profile
@@ -14,6 +14,66 @@ from authentication.exceptions import (
     InvitationAlreadyVerifiedError,
 )
 from authentication.tasks import task_send_message_invitable_action
+
+
+class InvitableInvitePermission(permissions.IsAuthenticated):
+
+    def has_object_permission(self, request, view, obj):
+        """Invite a Profile into this objects Profile list.
+
+        Profiles with elevated and above Invites can invite others.
+        Admins can invite anyone.
+        """
+        authenticated = super().has_object_permission(request, view, obj)
+        if not authenticated:
+            return False
+        if request.user.profile.type >= Profile.TYPES.admin:
+            return True
+        invite = obj.invites.filter(
+            profile_to=request.user.profile,
+        ).first()
+        return invite and invite.status >= Invitation.STATUS.elevated
+
+
+class InvitableInviteValidatePermission(permissions.IsAuthenticated):
+
+    def has_object_permission(self, request, view, obj):
+        """A Profile can only validate their own token."""
+        authenticated = super().has_object_permission(request, view, obj)
+        if not authenticated:
+            return False
+        if request.user.profile.type >= Profile.TYPES.admin:
+            return True
+        invite = obj.invites.filter(
+            profile_to=request.user.profile,
+        )
+        return invite and invite.status == Invitation.STATUS.invited
+
+
+class InvitableInviteRenewTokenPermission(permissions.IsAuthenticated):
+
+    def has_object_permission(self, request, view, obj):
+        """Invitations made can renew a token by the initiator.
+
+        Profiles with elevated invitation status can renew an Invitation they
+        issued to another Profile.
+        Admins can renew any Invitation.
+        """
+        authenticated = super().has_object_permission(request, view, obj)
+        if not authenticated:
+            return False
+        if request.user.profile.type >= Profile.TYPES.admin:
+            return True
+        invite = obj.invites.filter(
+            profile_to=request.user.profile,
+        ).first()
+        if not invite or invite.status < Invitation.STATUS.elevated:
+            return False
+        invite = obj.invites.filter(
+            profile=request.user.profile,
+            profile_to=request.data.get('profile_to'),
+        ).first()
+        return invite and invite.status == Invitation.STATUS.invited
 
 
 class InvitableViewSetMixin:
@@ -35,6 +95,7 @@ class InvitableViewSetMixin:
         )
 
     @detail_route(methods=['post'])
+    @permission_classes((InvitableInvitePermission, ))
     def invite(self, request, pk, **kwargs):
         """Invite a profile to another object."""
         inviting_to = self.get_object()
@@ -59,12 +120,13 @@ class InvitableViewSetMixin:
         )
 
     @detail_route(methods=['post'])
+    @permission_classes((InvitableInvitePermission, ))
     def invite_change(self, request, pk, **kwargs):
         """Change an invitation for a profile to a different type."""
         changing_for = self.get_object()
         try:
-            changing_for.invite_change(
-                Profile.objects.filter(user=request.user).first(),
+            invite = changing_for.invite_change(
+                request.user.profile,
                 Profile.objects.filter(
                     id=request.data.get('profile_to'),
                 ).first(),
@@ -75,7 +137,8 @@ class InvitableViewSetMixin:
             InvitationMissingError,
         ) as e:
             return self._invitation_error_handle(e)
-        task_send_message_invitable_action.delay('invite_change', changing_for)
+        task_send_message_invitable_action.delay(
+            'invite_change', changing_for, invite)
         return Response(
             {
                 'status': 'changed',
@@ -84,6 +147,7 @@ class InvitableViewSetMixin:
         )
 
     @detail_route(methods=['post'])
+    @permission_classes((InvitableInviteValidatePermission, ))
     def invite_validate(self, request, pk, **kwargs):
         """Validate a token of an Invitation and accept Invitation."""
         changing_for = self.get_object()
@@ -110,6 +174,7 @@ class InvitableViewSetMixin:
         )
 
     @detail_route(methods=['post'])
+    @permission_classes((InvitableInviteRenewTokenPermission, ))
     def invite_token_renew(self, request, pk, **kwargs):
         """Renew an invitations' token for acceptance.
 
@@ -124,7 +189,7 @@ class InvitableViewSetMixin:
         if not invite:
             return self._invitation_error_handle(
                 {'detail': _('Not Found'), },
-                error_status=status.HTTP_404_NOT_FOUND,
+                error_status=status.HTTP_403_FORBIDDEN,
             )
         try:
             invite.token_recreate()
@@ -140,17 +205,10 @@ class InvitableViewSetMixin:
         )
 
     @detail_route(methods=['get'])
+    @permission_classes((InvitableInvitePermission, ))
     def invites_withdrawn(self, request, pk, **kwargs):
         """List Invitations a Profile has withdrawn from."""
         inspecting = self.get_object()
-        if not inspecting.invites.filter(
-                profile_to__id=request.user.profile.id,
-                status=Invitation.STATUSES.elevated,
-        ).first():
-            return self._invitation_error_handle(
-                {'detail': _('unauthorized'), },
-                error_status=status.HTTP_401_UNAUTHORIZED,
-            )
         return Response(
             {
                 'status': 'ok',
